a3e628f1a23e8592f93b5ecf1df853fc2ecc2425
Ziheng Liang <lianz@Zihengs-MacBook-Pro.local>
2017-10-02 17:20:16 -0400

    first assignment starter code

---
+++ Assignment1/build/CMakeFiles/voxelizer.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/lianz/Work/csc2521/Assignment1/source/CompFab.cpp" "/Users/lianz/Work/csc2521/Assignment1/build/CMakeFiles/voxelizer.dir/source/CompFab.cpp.o"
+  "/Users/lianz/Work/csc2521/Assignment1/source/Mesh.cpp" "/Users/lianz/Work/csc2521/Assignment1/build/CMakeFiles/voxelizer.dir/source/Mesh.cpp.o"
+  "/Users/lianz/Work/csc2521/Assignment1/source/main.cpp" "/Users/lianz/Work/csc2521/Assignment1/build/CMakeFiles/voxelizer.dir/source/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
